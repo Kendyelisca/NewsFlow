80c44c0b250adc9570634591f04b1cc68736e143
Kendy <kendyelisca5@gmail.com>
2023-11-08 09:03:40 -0500

    latest news added

---
+++ components/suscribe/Suscribe.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useState } from "react";
+import "./suscribe.css";
+
+const Subscribe = () => {
+  const [email, setEmail] = useState(""); // State to store the email input value
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value); // Update the email state when input changes
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent the page from reloading on form submission
+
+    // Here, add the logic to store the email value (e.g., send it to a server or save it to local storage)
+    console.log("Email submitted:", email);
+
+    // Clear the email input after submission
+    setEmail("");
+  };
+
+  return (
+    <div className="sus-container">
+      <b className="font-bold text-red-800">NewsFlow</b>
+      <p className="font-bold">
+        Let's subscribe so you don't miss the latest updates
+      </p>
+      <form className="sub-form pt-4" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Your email"
+          className="p-2 pr-24"
+          value={email} // Bind the input value to the email state
+          onChange={handleEmailChange} // Handle input changes
+        />
+        <button type="submit" className="bg-red-800 text-white p-2 pl-4 pr-4">
+          Subscribe
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Subscribe;
