31ea39b8b8e8f6e8316d2a987c7f10abc6865b6b
Kendy <kendyelisca5@gmail.com>
2023-11-15 14:37:20 -0500

    adjusted the form model

--- components/suscribe/Suscribe.jsx
+++ components/suscribe/Suscribe.jsx
@@ -1,31 +1,49 @@
 "use client";
 
 import { useState } from "react";
+import axios from "axios";
 import "./suscribe.css";
 
 const Subscribe = () => {
   const [email, setEmail] = useState(""); // State to store the email input value
+  const [submitted, setSubmitted] = useState(false); // State to track if the form has been submitted
+  const [errorMessage, setErrorMessage] = useState(""); // State to store error messages
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value); // Update the email state when input changes
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault(); // Prevent the page from reloading on form submission
 
-    // Here, add the logic to store the email value (e.g., send it to a server or save it to local storage)
-    console.log("Email submitted:", email);
+    try {
+      // Here, add the logic to send the email value to an API endpoint
+      const response = await axios.post("your-api-endpoint", { email });
+      console.log("API response:", response.data);
 
-    // Clear the email input after submission
-    setEmail("");
+      // Clear the email input after successful submission
+      setEmail("");
+      setSubmitted(true); // Set submitted to true
+      setErrorMessage(""); // Clear any previous error message
+    } catch (error) {
+      console.error("Error submitting form:", error);
+      setSubmitted(false); // Set submitted to false in case of an error
+      setErrorMessage("Please try again the 20th of November."); // Set an error message
+    }
   };
 
   return (
-    <div className="sus-container" id="suscribe">
+    <div className="sus-container" id="subscribe">
       <b className="font-bold text-red-800">NewsFlow</b>
       <p className="font-bold text-center">
         Let's subscribe so you don't miss the latest updates
       </p>
+
+      {submitted && !errorMessage && (
+        <p className="text-green-600">Subscription successful!</p>
+      )}
+      {errorMessage && <p className="text-red-600">{errorMessage}</p>}
+
       <form className="sub-form pt-4" onSubmit={handleSubmit}>
         <input
           type="email"
