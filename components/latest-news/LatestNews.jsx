1372de129cce6bd288d69a1ec66fdf9aea5ad549
Kendy <kendyelisca5@gmail.com>
2023-11-20 15:04:07 -0500

    optimized code

--- components/latest-news/LatestNews.jsx
+++ components/latest-news/LatestNews.jsx
@@ -5,11 +5,23 @@ import axios from "axios";
 import { formatDistanceToNow } from "date-fns";
 import "./latestNews.css";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 
 const LatestNews = () => {
   const [articles, setArticles] = useState([]);
   const apiKey = process.env.NEXT_PUBLIC_API_KEY;
   useEffect(() => {
+    // Fetch saved articles from your backend
+    const fetchSavedArticles = async () => {
+      try {
+        const response = await axios.get("/api/saved-articles");
+        setSavedArticles(response.data);
+      } catch (error) {
+        console.error("Error fetching saved articles: ", error);
+      }
+    };
+
     axios
       .get(
         `https://gnews.io/api/v4/top-headlines?category=general&lang=en&apikey=${apiKey}`
@@ -18,7 +30,37 @@ const LatestNews = () => {
         const articleData = response.data.articles.slice(0, 8); // Get the first 8 articles
         setArticles(articleData);
       });
+
+    fetchSavedArticles();
   }, []);
+
+  //const handleSaveClick = async (article) => {
+  // try {
+  // Check if the article is already saved
+  //if (
+  //    savedArticles.find((savedArticle) => savedArticle.url === article.url)
+  //  ) {
+  // Article is already saved, handle accordingly (e.g., show a message)
+  //console.log("Article is already saved");
+  // return;
+  //}
+
+  // Send a POST request to save the article
+  // await axios.post("/api/save-article", { article });
+
+  // Update the saved articles state
+  //setSavedArticles((prevSavedArticles) => [...prevSavedArticles, article]);
+  //} catch (error) {
+  //  console.error("Error saving article: ", error);
+  // Handle error (e.g., show an error message to the user)
+  //  }
+  //};
+
+  //const isArticleSaved = (article) => {
+  //return savedArticles.some(
+  //(savedArticle) => savedArticle.url === article.url
+  //    );
+  //};
 
   return (
     <div>
@@ -34,17 +76,29 @@ const LatestNews = () => {
         {articles.map((article) => (
           <div key={article.url} className="article-card">
             <a href={article.url} target="_blank">
-              {" "}
               <img src={article.image} alt={article.title} />
               <div className="article-details ">
                 <div className="flex gap-3 pb-3 justify-center">
-                  <p className="font-bold text-red-800">
+                  <p className="font-bold text-sm text-red-800">
                     {article.source.name}
                   </p>
                   <b>.</b>
-                  <p>
+                  <p className="text-sm">
                     {formatDistanceToNow(new Date(article.publishedAt))} ago
                   </p>
+                  <FontAwesomeIcon
+                    icon={faBookmark}
+                    className="save-icon"
+                    // icon={
+                    // isArticleSaved(article)
+                    //  ? ["fas", "bookmark"]
+                    // : ["far", "bookmark"]
+                    // }
+                    //className={
+                    //isArticleSaved(article) ? "save-icon-red" : "save-icon"
+                    // }
+                    // onClick={() => handleSaveClick(article)}
+                  />
                 </div>
                 <h3 className="font-bold">{article.title}</h3>
               </div>
